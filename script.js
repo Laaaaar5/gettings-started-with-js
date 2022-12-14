d71c6d5be5be5f12608bab68c5bfc97853aad240
Lars Schürmann <laaaaar5@gmail.com>
2022-12-14 15:01:43 +0100

    Task "Net and Gross Price"

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ console.log(myName);
 
 const greeting = "Hallo";
 console.log(greeting + " " + myName);
+
+const netPrice = 25;
+const tax = 19;
+let grossPrice = (netPrice * (100 + tax)) / 100;
+console.log(grossPrice);
